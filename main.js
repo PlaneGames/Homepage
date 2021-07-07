24ced917b1e6547789d19593f0a9d70e219eaf0b
PlaneGames <multigeal@naver.com>
2021-07-08 02:37:47 +0900

    Slider Animation Upgrade

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var target = document.getElementsByClassName("port-con")[0];    //배열의 주소를 반환하므로 해당 인자를 지정해야 한다.
 var target2 = document.getElementsByClassName("port-img")[0];
-const styles = {
-    transition: 'all 1s ease-out'
-};
+
+
 if (window.addEventListener)
     window.addEventListener('DOMMouseScroll', wheel, false);
 window.onmousewheel = document.onmousewheel = wheel;
@@ -16,22 +15,17 @@ var aa = 0;
 
 // 마우스 휠~
 function handle(delta) {
-    var s = delta + ": ";
     if (delta > 0) {
-        
-        target.style.opacity = ".1";
-        //left: 50%;
-        //transform: translate(-50%, 0%);
-        aa += 100;
-        target.style.marginLeft = String(aa) + "px";
-        callback
+        if (aa < 0) {
+            aa += 240;
+            target.style.marginLeft = String(aa) + "px";
+        }
     }
     else {
-        target.style.opacity = "1";
-        target.style.marginLeft = "20px";
-        
-        aa -= 100;
-        target.style.marginLeft = String(aa) + "px";
+        if (aa > -3360) {
+            aa -= 240;
+            target.style.marginLeft = String(aa) + "px";
+        }
     }
 }
 
