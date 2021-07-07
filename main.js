dabc7c8aeeb785054035e2ff533eec66bb6943ef
PlaneGames <multigeal@naver.com>
2021-07-07 18:04:08 +0900

    JS Test & images upsizing

---
+++ main.js
@@ -0,0 +1,6 @@
+var target = document.getElementsByClassName("port-con")[0];    //배열의 주소를 반환하므로 해당 인자를 지정해야 한다.
+var target2 = document.getElementsByClassName("port-img")[0];
+
+target.style.opacity = ".1";
+target2.style.left = "1";
+target2.style.transform = "translate(1%, 0%)";
