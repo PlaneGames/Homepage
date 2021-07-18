2dea3b03c9e7b327fab214229c902ab2d778e007
PlaneGames <multigeal@naver.com>
2021-07-19 05:21:58 +0900

    Text Animation Update + lettering

--- src/TweenMax.textani.js
+++ src/TweenMax.textani.js
@@ -1,6 +1,37 @@
-TweenLite.to('#class2', 2, {
-  left: 100,
-  top: 75,
-  backgroundColor:"#000",
-  ease: Power4.easeIn
+$(document).ready(function() {
+  $(".po-text-ani").lettering();
+  $(".button").lettering();
 });
+
+
+function animation() {
+  var title1 = new TimelineMax();
+  title1.to(".button", 0, {
+    visibility: 'hidden', opacity: 0
+  });
+
+  title1.staggerFromTo(".po-text-ani span", 0.5, {
+    ease: Back.easeOut.config(1.7),
+    opacity: 0,
+    bottom: -80
+  },
+  {
+    ease: Back.easeOut.config(1.7),
+    opacity: 1,
+    bottom: 0
+  }, 0.1);
+
+  title1.to(".button", 0.2, {
+    visibility: 'visible', opacity: 1
+  });
+}
+
+
+$(document).ready(function() {
+  animation();
+}, 0);
+
+$('.button').click(function() {
+  animation();
+});
+
