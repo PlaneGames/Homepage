eb662b1e370dbfcbb1bca94133477975e38761d9
PlaneGames <multigeal@naver.com>
2021-07-21 06:38:17 +0900

    Portfolio Page Change System Update

--- main.js
+++ main.js
@@ -44,9 +44,9 @@ function wheel(event){
     if (delta) handle(delta);
 }
 
-var timeout;
-
 (function ($) {
+
+    var timeout;
 
     function resizing_overlay() {
         if (window.innerWidth*(66.675/100) <= window.innerHeight) {
@@ -72,26 +72,16 @@ var timeout;
 
     $(document).ready(function() {
 
-        var resetPopup = function(index) {
+        var openProjectPopup = function(index) {
 
-            $('.port-img')
-                .css('min-width','10vw')
-                .css('height','20vw')
+            var nth_index = "nth-of-type("+(index+1)+")";
+
+            clearTimeout(timeout);
+            $('.port-img').not(".port-img:"+nth_index)
                 .css('opacity','.0')
                 .css('filter','sepia(80%)')
                 .css('transition','.5s')
                 .css('border-radius','16px');
-
-            $(index)
-                .css('min-width','10vw')
-                .css('height','20vw')
-                .css('opacity','1')
-                .css('filter','sepia(0%)')
-                .css('transition','.5s')
-                .css('border-radius','16px');
-
-            clearTimeout(timeout);
-
             $('.port-header')
                 .css('margin-bottom','-120px')
                 .css('padding-bottom','0px')
@@ -99,98 +89,45 @@ var timeout;
                 .css('transition','.5s');
 
             timeout = setTimeout(function() {
-                $('.port-header').hide();
+                $(".port-img:"+nth_index)
+                    .css('width','0px')
+                    .css('min-width','0px')
+                    .css('max-width','0px')
+                    .css('transition','1s');
+                $('.po-con:'+nth_index).show();
             }, 500);
-            $('.po-con:nth-of-type(1)').show();
-        }
 
-        var setPopup = function(index) {
-
-            if (subpage == 0) {
-                animation();
-                /*
-                $('body')
-                    .css('background-color','rgb(225, 224, 222)');
-                    */
+            timeout = setTimeout(function() {
                 $('body')
                     .css('background-color','rgb(81, 99, 94)')
                     .css('transition','1s');
-                $('.po-con')
-                    .css('opacity','1')
-                    .css('transition','1s');
-                $('.po-con:nth-of-type(1)')
+                $('.po-con:'+nth_index)
                     .css('opacity','1')
                     .css('transform','translate(-50%, -50%)')
                     .css('transition','2s');
-
-                timeout = setTimeout(function() {
-                    $('.po-con:nth-of-type(1)')
-                        .css('transition','.0s');
-                }, 500);
-
-                $(index)
-                    .css('opacity','0')
-                    .css('filter','sepia(0%)')
-                    .css('transition','.5s')
-                    .css('border-radius','0px')
-                subpage++;
-                $('.port-imgcon').hide()
-            } else if (subpage == 1) {
-                subpage++;
-            }
-            
-        }
-
-        var aaa = function(index) {
-
-            resetPopup(index);
+            }, 1000);
 
             timeout = setTimeout(function() {
-                setPopup(index);
-            }, 0);
+                animation();
+                $('.po-con:'+nth_index)
+                    .css('transition','0s');
+            }, 1500);
 
         }
 
-        $(".port-img:nth-of-type(1)").on('click', function(){
-            aa = 42;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(2)").on('click', function(){
-            aa = 42 - 12;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(3)").on('click', function(){
-            aa = 42 - 12*2;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(4)").on('click', function(){
-            aa = 42 - 12*3;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(5)").on('click', function(){
-            aa = 42 - 12*4;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(6)").on('click', function(){
-            aa = 42 - 12*5;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(7)").on('click', function(){
-            aa = 42 - 12*6;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
-        $(".port-img:nth-of-type(8)").on('click', function(){
-            aa = 42 - 12*7;
-            target.style.marginLeft = String(aa) + "vw";
-            aaa(this);
-        });
+        var galleryButton = function(index) {
+            $(".port-img:nth-of-type("+(index+1)+")").on('click', function(){
+                aa = 42 - 12*index;
+                target.style.marginLeft = String(aa) + "vw";
+                openProjectPopup(index);
+                //console.log(index); //console test
+            });
+        }
+
+        for(var i = 0; i < 8; i ++) {
+            galleryButton(i);
+        }
+
     });
 
 })(jQuery);
